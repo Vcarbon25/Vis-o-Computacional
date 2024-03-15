02be18c19e7c2242e3412f0eb9670f841034ec9f
Victor Carbón Ferreira <92864869+Vcarbon25@users.noreply.github.com>
2024-03-15 10:31:42 -0300

    camera com PySimpleGui

--- camera.py
+++ camera.py
@@ -1,34 +1,17 @@
 import cv2
-import time
-import tkinter as TK
-from PIL import Image, ImageTk
+import PySimpleGUI as sg
 
-raiz = TK.Tk()
+row1 = [[sg.Image(filename="",key="camera")]]
+layout=[row1]
+cap = cv2.VideoCapture(0)       # Setup the camera as a capture device
+janela=sg.Window('V8DoTCC Mediapipe ',layout).finalize()
 
-ECamera =TK.Label(raiz)
-ECamera.grid(row=1, column=0)
+while True:                     # The PSG “Event Loop”
 
-ptime=0
-ctime=0
-cap= cv2.VideoCapture(0)
+    event, values = janela.Read(timeout=20, timeout_key='timeout')      # get events for the window with 20ms max wait
+    sucess, frame = cap.read()
+        
+    janela['camera'].Update(data=cv2.imencode('.png', frame)[1].tobytes()) # Update image in window
+    if event==sg.WINDOW_CLOSED:
+        break
 
-def show_frames():  #Não sei porque no arquivo novo funciona e no velho não
-   # Get the latest frame and convert into Image
-   cv2image= cv2.cvtColor(cap.read()[1],cv2.COLOR_BGR2RGB)
-   ctime = time.time()
-   global ptime
-   fps=1/(ctime-ptime)
-   nfps = int(fps)
-   txtfps=str(nfps)
-   cv2.putText(cv2image, txtfps,(10,50),cv2.FONT_HERSHEY_SCRIPT_COMPLEX,2,(20,80,255))
-   ptime=ctime #ele conseguiu fazer a contagem de fps aqui no tkinter tbm
-   img = Image.fromarray(cv2image)
-   # Convert image to PhotoImage
-   imgtk = ImageTk.PhotoImage(image = img)
-   ECamera.imgtk = imgtk
-   ECamera.configure(image=imgtk)
-   # Repeat after an interval to capture continiously
-   ECamera.after(20, show_frames)
-
-show_frames()
-raiz.mainloop()
